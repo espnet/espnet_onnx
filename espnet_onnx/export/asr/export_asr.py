bc477d1476aee099fb1e17babc02d4c8bfa4ded9
Masao-Someki <masao.someki@outlook.jp>
2022-03-21 01:55:08 +0900

    Add RNNDecoder export feature
    - This RNNDecoder only supports `AttLoc` as attention layer.
    - Since execution of a layer in attention layer is separated from other decoder layers, I split that layer as predecoder model.

--- espnet_onnx/export/asr/export_asr.py
+++ espnet_onnx/export/asr/export_asr.py
@@ -15,7 +15,9 @@ from espnet2.bin.asr_inference import Speech2Text
 from espnet2.text.sentencepiece_tokenizer import SentencepiecesTokenizer
 from .models import (
     get_encoder,
-    Decoder,
+    get_decoder,
+    RNNDecoder,
+    PreDecoder,
     CTC,
     LanguageModel
 )
@@ -61,7 +63,7 @@ class ModelExport:
             model.asr_model, export_dir))
 
         # export decoder
-        dec_model = Decoder(model.asr_model.decoder)
+        dec_model = get_decoder(model.asr_model.decoder)
         self._export_decoder(dec_model, enc_out_size, export_dir)
         model_config.update(decoder=dec_model.get_model_config(export_dir))
 
@@ -83,7 +85,11 @@ class ModelExport:
             quantize_dir.mkdir(exist_ok=True)
             qt_config = self._quantize_model(export_dir, quantize_dir)
             for m in qt_config.keys():
-                model_config[m].update(quantized_model_path=qt_config[m])
+                if 'predecoder' in m:
+                    model_idx = int(m.split('_')[1])
+                    model_config['decoder']['predecoder'][model_idx].update(quantized_model_path=qt_config[m])
+                else:
+                    model_config[m].update(quantized_model_path=qt_config[m])
 
         config_name = base_dir / 'config.yaml'
         save_config(model_config, config_name)
@@ -136,6 +142,24 @@ class ModelExport:
             output_names=dec_model.get_output_names(),
             dynamic_axes=dec_model.get_dynamic_axes()
         )
+        # if decoder is RNNDecoder, then export predecoders
+        if isinstance(dec_model, RNNDecoder):
+            self._export_predecoder(dec_model, path)
+    
+    def _export_predecoder(self, dec_model, path):
+        for i, att in enumerate(dec_model.model.att_list):
+            att_model = PreDecoder(att)
+            file_name = os.path.join(path, 'predecoder_%d.onnx' % i)
+            torch.onnx.export(
+                att_model,
+                att_model.get_dummy_inputs(),
+                file_name,
+                verbose=True,
+                opset_version=11,
+                input_names=att_model.get_input_names(),
+                output_names=att_model.get_output_names(),
+                dynamic_axes=att_model.get_dynamic_axes()
+            )
 
     def _export_ctc(self, ctc_model, enc_size, path):
         file_name = os.path.join(path, 'ctc.onnx')
