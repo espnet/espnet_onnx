d3c1b90ad575ac5ddaa2b043f10893a92f736314
Masao-Someki <masao.someki@gmail.com>
2022-05-06 11:08:52 +0900

    Renamed ModelExport into ASRModelExport and added TTSModelExport

--- espnet_onnx/export/tts/export_tts.py
+++ espnet_onnx/export/tts/export_tts.py
@@ -12,33 +12,28 @@ import numpy as np
 import torch
 from onnxruntime.quantization import quantize_dynamic, QuantType
 
-from espnet2.bin.asr_inference import Speech2Text
+from espnet2.bin.tts_inference import Text2Speech
 from espnet2.text.sentencepiece_tokenizer import SentencepiecesTokenizer
 from espnet_model_zoo.downloader import ModelDownloader
 from .models import (
-    get_encoder,
-    get_decoder,
-    RNNDecoder,
-    PreDecoder,
-    CTC,
-    LanguageModel,
-    JointNetwork,
+    get_model,
+    get_vocoder,
 )
-from .get_config import (
-    get_ngram_config,
-    get_beam_config,
-    get_token_config,
-    get_tokenizer_config,
-    get_weights_transducer,
-    get_trans_beam_config,
-)
+# from .get_config import (
+#     get_ngram_config,
+#     get_beam_config,
+#     get_token_config,
+#     get_tokenizer_config,
+#     get_weights_transducer,
+#     get_trans_beam_config,
+# )
 from espnet_onnx.utils.config import (
     save_config,
     update_model_path
 )
 
 
-class ASRModelExport:
+class TTSModelExport:
     def __init__(self, cache_dir: Union[Path, str] = None):
         assert check_argument_types()
         if cache_dir is None:
@@ -67,46 +62,16 @@ class ASRModelExport:
         model_config = self._create_config(model, export_dir)
 
         # export encoder
-        enc_model = get_encoder(model.asr_model.encoder)
-        enc_out_size = enc_model.get_output_size()
-        self._export_encoder(enc_model, export_dir, verbose)
-        model_config.update(encoder=enc_model.get_model_config(
+        tts_model = get_model(model.model)
+        self._export_tts(tts_model, export_dir, verbose)
+        model_config.update(tts_model=enc_model.get_model_config(
             model.asr_model, export_dir))
 
-        # export decoder
-        dec_model = get_decoder(model.asr_model.decoder)
-        self._export_decoder(dec_model, enc_out_size, export_dir, verbose)
-        model_config.update(decoder=dec_model.get_model_config(export_dir))
-        
-        # export joint_network if transducer decoder is used.
-        if model.asr_model.use_transducer_decoder:
-            joint_network = JointNetwork(
-                model.asr_model.joint_network,
-                model_config['beam_search']['search_type'],
-            )
-            self._export_joint_network(joint_network, export_dir, verbose)
-            model_config.update(joint_network=joint_network.get_model_config(export_dir))
-
-        # export ctc
-        ctc_model = CTC(model.asr_model.ctc.ctc_lo)
-        self._export_ctc(ctc_model, enc_out_size, export_dir, verbose)
-        model_config.update(ctc=ctc_model.get_model_config(export_dir))
-
-        # export lm
-        export_lm = False
-        if not model.asr_model.use_transducer_decoder:
-            if 'lm' in model.beam_search.full_scorers.keys():
-                export_lm = True
-        else:
-            if model.beam_search_transducer.use_lm:
-                export_lm = True
-        
-        if export_lm:
-            lm_model = LanguageModel(model.beam_search.full_scorers['lm'])
-            self._export_lm(lm_model, export_dir, verbose)
-            model_config.update(lm=lm_model.get_model_config(export_dir))
-        else:
-            model_config.update(lm=dict(use_lm=False))
+        # export vocoder
+        if model.vocoder is not None:
+            voc_model = get_vocoder(model.vocoder)
+            self._export_vocoder(voc_model, export_dir, verbose)
+            model_config.update(vocoder=voc_model.get_model_config(export_dir))
 
         if quantize:
             quantize_dir = base_dir / 'quantize'
@@ -124,17 +89,13 @@ class ASRModelExport:
         save_config(model_config, config_name)
         update_model_path(tag_name, base_dir)
 
-    def export_from_pretrained(self, tag_name: str, quantize: bool = False):
+    def export_from_pretrained(self, tag_name: str = None, zip_file: str = None, quantize: bool = False):
         assert check_argument_types()
-        model = Speech2Text.from_pretrained(tag_name)
-        self.export(model, tag_name, quantize)
-    
-    def export_from_zip(self, path: Union[Path, str], tag_name: str, quantize: bool = False):
-        assert check_argument_types()
-        cache_dir = Path(path).parent
-        d = ModelDownloader(cache_dir)
-        model_config = d.unpack_local_file(path)
-        model = Speech2Text(**model_config)
+        if ((tag_name is not None) and (zip_file is not None)) \
+            or ((tag_name is None) and (zip_file is None)):
+            raise RuntimeError('You should specify value for one of ["tag_name", "zip_file"]')
+        _t = tag_name if tag_name is not None else zip_file
+        model = Text2Speech.from_pretrained(_t)
         self.export(model, tag_name, quantize)
 
     def _create_config(self, model, path):
@@ -177,13 +138,13 @@ class ASRModelExport:
             dynamic_axes=model.get_dynamic_axes()
         )
 
-    def _export_encoder(self, model, path, verbose):
+    def _export_tts(self, model, path, verbose):
         file_name = os.path.join(path, 'encoder.onnx')
         if verbose:
             logging.info(f'Encoder model is saved in {file_name}')
         self._export_model(model, file_name, verbose)
 
-    def _export_decoder(self, model, enc_size, path, verbose):
+    def _export_vocoder(self, model, enc_size, path, verbose):
         file_name = os.path.join(path, 'decoder.onnx')
         if verbose:
             logging.info(f'Decoder model is saved in {file_name}')
@@ -192,60 +153,6 @@ class ASRModelExport:
         # if decoder is RNNDecoder, then export predecoders
         if isinstance(model, RNNDecoder):
             self._export_predecoder(model, path, verbose)
-
-    def _export_predecoder(self, model, path, verbose):
-        if verbose:
-            logging.info(f'Pre-Decoder model is saved in {path}.' \
-                + f'There should be {len(model.model.att_list)} files.')
-            
-        for i, att in enumerate(model.model.att_list):
-            att_model = PreDecoder(att)
-            if att_model.require_onnx():
-                file_name = os.path.join(path, f'predecoder_{i}.onnx')
-                self._export_model(att_model, file_name, verbose)
-
-    def _export_ctc(self, model, enc_size, path, verbose):
-        file_name = os.path.join(path, 'ctc.onnx')
-        if verbose:
-            logging.info(f'CTC model is saved in {file_name}')
-        self._export_model(model, file_name, verbose, enc_size)
-
-    def _export_lm(self, model, path, verbose):
-        file_name = os.path.join(path, 'lm.onnx')
-        if verbose:
-            logging.info(f'LM model is saved in {file_name}')
-        self._export_model(model, file_name, verbose)
-    
-    def _export_joint_network(self, model, path, verbose):
-        file_name = os.path.join(path, 'joint_network.onnx')
-        if verbose:
-            logging.info(f'JointNetwork model is saved in {file_name}')
-        self._export_model(model, file_name, verbose)
-        
-    def _copy_files(self, model, path, verbose):
-        # copy stats file
-        if model.asr_model.normalize is not None \
-                and hasattr(model.asr_model.normalize, 'stats_file'):
-            stats_file = model.asr_model.normalize.stats_file
-            shutil.copy(stats_file, path)
-            if verbose:
-                logging.info(f'`stats_file` was copied into {path}.')
-
-        # copy bpemodel
-        if isinstance(model.tokenizer, SentencepiecesTokenizer):
-            bpemodel_file = model.tokenizer.model
-            shutil.copy(bpemodel_file, path)
-            if verbose:
-                logging.info(f'bpemodel was copied into {path}.')
-            
-        # save position encoder parameters.
-        if hasattr(model.asr_model.encoder, 'pos_enc'):
-            np.save(
-                path / 'pe',
-                model.asr_model.encoder.pos_enc.pe.numpy()
-            )
-            if verbose:
-                logging.info(f'Matrix for position encoding was copied into {path}.')
 
     def _quantize_model(self, model_from, model_to, verbose):
         if verbose:
