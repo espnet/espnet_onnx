10b8d2ef14b85dda04c4dca50f428bc16da91c13
Masao-Someki <masao.someki@gmail.com>
2022-06-10 03:37:09 +0900

    released 0.1.8

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = {
 
 setup(
     name="espnet_onnx",
-    version="0.1.7",
+    version="0.1.8",
     url="https://github.com/Masao-Someki/espnet_onnx",
     author="Masao Someki",
     author_email="masao.someki@gmail.com",
