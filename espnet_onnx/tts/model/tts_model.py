ec92495c9e63729c8cca3806f582204794401845
Masao-Someki <masao.someki@gmail.com>
2022-06-27 01:02:11 +0900

    Add JETS

--- espnet_onnx/tts/model/tts_model.py
+++ espnet_onnx/tts/model/tts_model.py
@@ -1,5 +1,6 @@
 
 from .tts_models.vits import VITS
+from .tts_models.jets import JETS
 from .tts_models.fast_speech2 import FastSpeech2
 from .tts_models.tacotron2 import Tacotron2
 
@@ -7,6 +8,8 @@ from .tts_models.tacotron2 import Tacotron2
 def get_tts_model(config, providers, use_quantized):
     if config.model_type == 'VITS':
         return VITS(config, providers, use_quantized)
+    elif config.model_type == 'JETS':
+        return JETS(config, providers, use_quantized)
     elif config.model_type == 'FastSpeech2':
         return FastSpeech2(config, providers, use_quantized)
     elif config.model_type == 'Tacotron2':
