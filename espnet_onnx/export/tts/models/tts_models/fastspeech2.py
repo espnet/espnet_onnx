0f112d22c22ef3ab1fa93520edd7d33a7da5741c
Masao-Someki <masao.someki@gmail.com>
2022-06-29 01:18:49 +0900

    Fixed LengthRegulator

--- espnet_onnx/export/tts/models/tts_models/fastspeech2.py
+++ espnet_onnx/export/tts/models/tts_models/fastspeech2.py
@@ -15,32 +15,21 @@ from espnet_onnx.utils.abs_model import AbsExportModel
 
 
 class OnnxLengthRegurator(nn.Module):
-    def __init__(self, alpha):
+    def __init__(self, alpha=1.0):
         super().__init__()
         self.alpha = alpha
         # The maximum length of the make_pad_mask is the 
         # maximum value of the duration.
         self.make_pad_mask = MakePadMask(512)
-        self.zero = torch.LongTensor([0])
-    
     def forward(self, x, dur):
         # This class assumes that the batch size of x
         # should be 1.
         if self.alpha != 1.0:
             dur = torch.round(ds.float() * alpha).long()
         dur = dur[0]
-        ma = torch.cumsum(
-            torch.cat((dur, self.zero), dim=0),
-            dim=0
-        )
-        mb = torch.cumsum(
-            torch.cat((self.zero, dur), dim=0),
-            dim=0
-        )
-        _m = self.make_pad_mask(mb)
-        _m[0] = 1
-        mask = ((1 - self.make_pad_mask(ma)) * _m)[:-1]
-        return torch.matmul(x.transpose(1, 2), mask).transpose(1, 2)
+        dm = 1 - self.make_pad_mask(dur)
+        nz = torch.nonzero(dm)[:, 0]
+        return x[:, nz]
 
 
 class OnnxReferenceEncoder(nn.Module):
