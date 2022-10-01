a80e5cc300fd7bd26db98d20fe31678fafc1b84b
Masao Someki <masao.someki@gmail.com>
2022-10-01 16:13:20 +0900

    Moved op_test_utils

--- tests/unit_tests/test_optimize.py
+++ tests/unit_tests/test_optimize.py
@@ -9,7 +9,7 @@ import glob
 import onnx
 
 from espnet_onnx.export.optimize.optimizer import optimize_model
-from .op_test_utils import check_op_type_count
+from ..op_test_utils import check_op_type_count
 
 test_cases = [
     ['encoder', 'transformer', 4, 256, 'Attention', 3, False, False],
