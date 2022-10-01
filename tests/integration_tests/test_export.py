9f93a258b13300cade9a20faf6b6ff306f61e9fc
Masao Someki <masao.someki@gmail.com>
2022-10-01 19:59:46 +0900

    Add export test

--- tests/integration_tests/test_export.py
+++ tests/integration_tests/test_export.py
@@ -2,6 +2,7 @@
 # model correctly and match the result.
 
 import os
+import shutil
 import glob
 import pytest
 from pathlib import Path
@@ -10,166 +11,59 @@ import numpy as np
 from ..op_test_utils import check_op_type_count
 
 
-asr_tag_names = [
-    # RNN encoder and RNN decoder
-    {
-        'tag_name': 'kamo-naoyuki/timit_asr_train_asr_raw_word_valid.acc.ave',
-        'optimization': {'CPU': {}, 'GPU': {}},
-        'export_model_types': ['encoder', 'decoder', 'ctc']
-    },
-
-    # Transformer encoder and decoder
-    {
-        'tag_name': 'Shinji Watanabe/librispeech_asr_train_asr_transformer_e18_raw_bpe_sp_valid.acc.best',
-        'optimization': {
-            'CPU': {
-                'encoder': {
-                    'Attention': 12,
-                },
-                'decoder': {
-                    'CrossAttention': 6
-                },
-                'lm': {
-                    'CrossAttention': 12
-                }
-            },
-            'Quantize': {
-                'encoder': {
-                    'QAttention': 12,
-                },
-                'decoder': {
-                    'QCrossAttention': 6
-                },
-                'lm': {
-                    'QCrossAttention': 12
-                }
-            },
-            'GPU': {
-                'encoder': {
-                    'Attention': 12
-                }
-            }
-        },
-        'export_model_types': ['encoder', 'decoder', 'ctc', 'lm']
-    },
-
-    # Conformer encoder and decoder
-    {
-        'tag_name': 'pyf98/librispeech_conformer_hop_length160',
-        'optimization': {
-            'CPU': {
-                'encoder': {
-                    'RelPosAttention': 12
-                },
-                'decoder': {
-                    'CrossAttention': 6,
-                },
-                'lm': {
-                    'CrossAttention': 12
-                }
-            },
-            'Quantize': {
-                'encoder': {
-                    'QRelPosAttention': 12,
-                },
-                'decoder': {
-                    'QCrossAttention': 6
-                },
-                'lm': {
-                    'QCrossAttention': 12
-                }
-            },
-            'GPU': {
-                'encoder': {
-                    'RelativeShift': 12
-                }
-            }
-        },
-        'export_model_types': ['encoder', 'decoder', 'ctc', 'lm']
-    }
+asr_config_names = [
+    'original/rnn',
+    'original/transformer',
+    'original/conformer',
+    'custom/conformer_cpu',
+    'custom/conformer_gpu',
 ]
 
 
-
-def check_models(cache_dir, tag_name, export_model_types):
+def check_models(cache_dir, tag_name, check_export):
     file_paths = {'full': {}, 'quantize': {}}
     # test full model
-    for emt in export_model_types:
+    for emt in check_export:
         # test if models are properly exported
-        test_path = glob.glob(os.path.join(cache_dir, tag_name, 'full', f'*{emt}.onnx'))
+        test_path = glob.glob(os.path.join(cache_dir, tag_name.replace(' ', '-'), 'full', f'*{emt}.onnx'))
         assert len(test_path) == 1
         file_paths['full'][emt] = test_path[0]
 
     # test quantized model
-    for emt in export_model_types:
+    for emt in check_export:
         # test if models are properly exported
-        test_path = glob.glob(os.path.join(cache_dir, tag_name, 'quantize', f'*{emt}_qt.onnx'))
+        test_path = glob.glob(os.path.join(cache_dir, tag_name.replace(' ', '-'), 'quantize', f'*{emt}_qt.onnx'))
         assert len(test_path) == 1
         file_paths['quantize'][emt] = test_path[0]
 
     return file_paths
 
 
-@pytest.mark.parametrize('model', asr_tag_names)
-def test_asr_cpu_orig(model, model_export):
+@pytest.mark.parametrize('config', asr_config_names)
+def test_asr_cpu_orig(config, load_config, model_export):
+    model = load_config(config, model_type='integration')
     # test exportation for CPU with original onnxruntime
     model_export.set_export_config(
         max_seq_len=5000,
-        use_ort_for_espnet=False,
+        use_ort_for_espnet=model['use_ort_for_espnet'],
+        use_gpu=('GPU' in model['device'])
     )
     model_export.export_from_pretrained(
         model['tag_name'],
-        quantize=True,
+        quantize=('Quantize' in model['device']),
         optimize=True
     )
-    file_paths = check_models(model_export.cache_dir, model['tag_name'],model['export_model_types'])
-    if 'encoder' in model['export_model_types'] and 'encoder' in model['optimization']['CPU'].keys():
-        check_op_type_count(
-            file_paths['encoder'], **model['optimization']['CPU']['encoder']
-        )
-    if 'encoder' in model['export_model_types'] and 'encoder' in model['optimization']['Quantize'].keys():
-        check_op_type_count(
-            file_paths['encoder'], **model['optimization']['Quantize']['encoder']
-        )
+    file_paths = check_models(model_export.cache_dir, model['tag_name'], model['check_export'])
 
-@pytest.mark.parametrize('models', asr_tag_names)
-def test_asr_cpu_custom(models, model_export):  
-    # test exportation for CPU with original onnxruntime
-    model_export.set_export_config(
-        max_seq_len=5000,
-        use_ort_for_espnet=True,
-    )
-    model_export.export_from_pretrained(
-        model['tag_name'],
-        quantize=True,
-        optimize=True
-    )
-    file_paths = check_models(model_export.cache_dir, model['tag_name'],model['export_model_types'])
-    for k in  model['optimization']['CPU'].keys():
-        check_op_type_count(
-            file_paths[k], **model['optimization']['CPU'][k]
-        )
-    for k in  model['optimization']['Quantize'].keys():
-        check_op_type_count(
-            file_paths[k], **model['optimization']['Quantize'][k]
-        )
-
-@pytest.mark.parametrize('models', asr_tag_names)
-def test_asr_gpu_custom(models, model_export):  
-    # test exportation for CPU with original onnxruntime
-    model_export.set_export_config(
-        max_seq_len=5000,
-        use_ort_for_espnet=True,
-        use_gpu=True
-    )
-    model_export.export_from_pretrained(
-        model['tag_name'],
-        quantize=True,
-        optimize=True
-    )
-    file_paths = check_models(model_export.cache_dir, model['tag_name'],model['export_model_types'])
-    for k in  model['optimization']['GPU'].keys():
-        check_op_type_count(
-            file_paths[k], **model['optimization']['GPU'][k]
-        )
-
+    for device in model['device']:
+        if device is not None:
+            if device == 'Quantize':
+                for k in  model['optimization']['Quantize'].keys():
+                    check_op_type_count(
+                        file_paths['quantize'][k], model['optimization']['Quantize'][k]
+                    )
+            else:
+                for k in  model['optimization'][device].keys():
+                    check_op_type_count(
+                        file_paths['full'][k], model['optimization'][device][k]
+                    )
