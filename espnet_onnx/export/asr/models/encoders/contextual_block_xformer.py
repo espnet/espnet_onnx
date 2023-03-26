d6ce62f38d22bd1790725b34c2195e8822032695
Masao-Someki <masao.someki@gmail.com>
2023-03-26 14:02:46 +0900

    Applied padding before decoder layers
    - Padding is required for the final inference

--- espnet_onnx/export/asr/models/encoders/contextual_block_xformer.py
+++ espnet_onnx/export/asr/models/encoders/contextual_block_xformer.py
@@ -117,8 +117,13 @@ class ContextualBlockXformerEncoder(nn.Module, AbsExportModel):
             addin = addin * self.xscale + pos_enc_addin
 
         prev_addin = torch.cat([prev_addin, addin], dim=1)[:, is_first[0]].unsqueeze(1)
-        xs_pad = xs_pad * self.xscale + pos_enc_xs
-        ys_chunk = torch.cat([prev_addin, xs_pad, addin], dim=1).unsqueeze(1)
+
+        xs_pad = xs_pad * self.xscale + pos_enc_xs[:, :xs_pad.size(1)]
+
+        xs_chunk = torch.zeros(xs_pad.size(0), self.block_size, xs_pad.size(2))
+        xs_chunk[:, :xs_pad.size(1)] = xs_pad
+        
+        ys_chunk = torch.cat([prev_addin, xs_chunk, addin], dim=1).unsqueeze(1)
 
         next_encoder_ctx = past_encoder_ctx * 0
 
