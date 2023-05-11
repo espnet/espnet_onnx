055fcd58658c238ccbe5566b648a425d352b37ca
Masao-Someki <masao.someki@gmail.com>
2023-05-11 19:26:09 +0900

    Update version to 1.11

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = {
 
 setup(
     name="espnet_onnx",
-    version="0.1.10",
+    version="0.1.11",
     url="https://github.com/Masao-Someki/espnet_onnx",
     author="Masao Someki",
     author_email="masao.someki@gmail.com",
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
